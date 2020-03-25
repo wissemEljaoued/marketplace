8044cc43aa26b3e776d9d614d22f1453ac84e970
Wajdi Makhlouf <makwajdi@gmail.com>
2020-03-25 14:21:41 +0100

    Ajout du serveur de configuration

---
+++ configuration-server/src/main/java/de/tekup/marketplace/ConfigurationServer.java
@@ -0,0 +1,15 @@
+package de.tekup.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigurationServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationServer.class, args);
+    }
+
+}
